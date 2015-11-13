6f600ec07e67c934550cbf0cbbaac486df00867d
Alexzhxy <zxyperfect@gmail.com>
2015-11-13 14:48:45 -0800

    a few changes to favorite

--- cloud/delegates/favorites.js
+++ cloud/delegates/favorites.js
@@ -1,95 +1,104 @@
 var _ = require('underscore');
-var Favorite = Parse.Object.extend('Favorite');
 var favorite_assembler = require('cloud/assemblers/favorite');
+var error_handler = require('cloud/error_handler');
+
+var Favorite = Parse.Object.extend('Favorite');
 var Restaurant = Parse.Object.extend('Restaurant');
 var Dish = Parse.Object.extend('Dish');
 var List = Parse.Object.extend('List');
-var error_handler = require('cloud/error_handler');
+
 
 exports.create = function(req, res){
 	var user = req.user;
 	var type = req.body['type'];
 	var objectId = req.body['object_id'];
+
 	if (user == undefined) {
 		var error = {};
-		error['message'] = 'Please sign in to perform this operation';
-		res.json(400, error);
-	} else {
-		if (!validateParameters(type, res)) {
-			return;
-		}
-		var favorite = new Favorite();
-		if (type === 'dish') {
-			var dish = new Dish();
-			dish.id = objectId;
-			favorite.set('type', type);
-			favorite.set('user', user);
-			favorite.set('dish', dish);
-		} else if (type === 'restaurant') {
-			var restaurant = new Restaurant();
-			restaurant.id = objectId;
-			favorite.set('type', type);
-			favorite.set('user', user);
-			favorite.set('restaurant', restaurant);
-		} else if (type === 'list') {
-			var list = new List();
-			list.id = objectId;
-			favorite.set('type', type);
-			favorite.set('user', user);
-			favorite.set('list', list);
-		}
-		favorite.save().then(function(result){
-			var fav = favorite_assembler.assemble(result);
-			var response = {};
-			response['result'] = fav;
-			res.json(201, response);
-		}, function(error){
-			error_handler.handle(error, {}, res);
-		});
+		error['message'] = 'User not found';
+		res.json(404, error);
+		return;
 	}
-}
 
-function validateParameters(type, res) {
-	if (type !== 'dish' && type !== 'restaurant' && type !== 'list') {
+	if (!validateParameters(type)) {
 		var error = {};
 		error['message'] = 'The parameter \'type\' has invalid value';
 		res.json(400, error);
-		return false;
+		return;
 	}
-	return true;
+
+	var favorite = new Favorite();
+	if (type === 'dish') {
+		var dish = new Dish();
+		dish.id = objectId;
+		favorite.set('type', type);
+		favorite.set('user', user);
+		favorite.set('dish', dish);
+	} else if (type === 'restaurant') {
+		var restaurant = new Restaurant();
+		restaurant.id = objectId;
+		favorite.set('type', type);
+		favorite.set('user', user);
+		favorite.set('restaurant', restaurant);
+	} else if (type === 'list') {
+		var list = new List();
+		list.id = objectId;
+		favorite.set('type', type);
+		favorite.set('user', user);
+		favorite.set('list', list);
+	}
+	favorite.save().then(function(result){
+		var favoriteRes = favorite_assembler.assemble(result);
+		var response = {};
+		response['result'] = favoriteRes;
+		res.json(201, response);
+	}, function(error){
+		error_handler.handle(error, {}, res);
+	});
+	
 }
 
 exports.find = function(req, res){
 	var type = req.query['type'];
 	var user = req.user;
 	if (user == undefined) {
-		res.json(401, 'user not recognized');
-	} else {
-		if (!validateParameters(type, res)) {
-			return;
-		}
-		var query = new Parse.Query(Favorite);
-		query.equalTo('user', user);
-		query.limit(10);
-		query.include('dish.from_restaurant');
-		query.include('restaurant');
-		query.include('list');
-		query.equalTo('type', type);
-		query.find().then(function(results) {
-			if (results != undefined && results.length > 0) {
-				var reviews = [];
-				_.each(results, function(result) {
-					var rev = favorite_assembler.assemble(result);
-					reviews.push(rev);
-				});
-				var response = {};
-				response['results'] = reviews;
-				res.json(200, response);
-			}
-		}, function(error) {
-			error_handler.handle(error, {}, res);
-		});
+		var error = {};
+		error['message'] = 'User not found';
+		res.json(404, error);
+		return;
+	} 
+
+	if (!validateParameters(type)) {
+		var error = {};
+		error['message'] = 'The parameter \'type\' has invalid value';
+		res.json(400, error);
+		return;
 	}
+
+	var query = new Parse.Query(Favorite);
+	query.equalTo('user', user);
+	query.equalTo('type', type);
+	query.limit(10);
+
+	query.include('dish.from_restaurant');
+	query.include('restaurant');
+	query.include('list');
+
+	query.find().then(function(results) {
+		if (results != undefined && results.length > 0) {
+			var reviews = [];
+			_.each(results, function(result) {
+				var rev = favorite_assembler.assemble(result);
+				reviews.push(rev);
+			});
+			var response = {};
+			response['results'] = reviews;
+			res.json(200, response);
+		}
+	}, function(error) {
+		error_handler.handle(error, {}, res);
+	});
+	
 }
 
 exports.delete = function(req, res){
@@ -118,4 +127,13 @@ exports.delete = function(req, res){
 	}, function(error){
 		error_handler.handle(error, {}, res);
 	});
+}
+
+
+
+function validateParameters(type) {
+	if (type !== 'dish' && type !== 'restaurant' && type !== 'list') {
+		return false;
+	}
+	return true;
 }
